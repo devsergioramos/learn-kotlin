b239806a701f1105e6d9fa0507e4f3beeb29cd81
Sergio Ramos <sergio.b.snp@gmail.com>
2024-04-03 22:35:41 -0400

    Types in Kotlin

---
+++ src/main/kotlin/types.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    /*
+    * String - textos
+    * byte - Números inteiros (-128 até 127)
+    * int - Inteiros (-2147483648 até 2147483647)
+    * long - Inteiros (etc...)
+    * float - Decimais (1.1234567 até 7 casas decimais)
+    * double - Decimais (etc...)
+    * boolean - true ou false
+    * */
+
+    val nome = "Sérgio Ramos" // String
+    val idade: Byte = 28 // Byte
+    val idadeDias = 10227 // Int
+    val idadeMilisegundos = 883612800000 // Long
+    val patrimonio = 800000.00f // Float
+    val patrimonio2 = 800000.0000000000 // Double
+}
