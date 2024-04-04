3ec7d536eb18d6ea9555f975e6b27307693ee0da
Sergio Ramos <sergio.b.snp@gmail.com>
2024-04-03 22:46:19 -0400

    op aritimeticos

---
+++ src/main/kotlin/op-aritmeticos.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+    var numero1 = 20
+    var numero2 = 8
+
+    // soma
+    println(numero1 + numero2)
+
+    // sub
+    println(numero1 - numero2)
+
+    // div
+    println(numero1 / numero2)
+
+    // mult
+    println(numero1 * numero2)
+
+    // mod
+    println(numero1 % numero2)
+
+    // incr
+    println(++numero1)
+
+    // decr
+    println(--numero2)
+}
