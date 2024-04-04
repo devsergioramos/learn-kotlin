04fc9017b159a798247140c322d22ce045ee079c
Sergio Ramos <sergio.b.snp@gmail.com>
2024-04-03 22:20:35 -0400

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    // var Mutável
+    var nome =  "Sergio Ramos"
+
+    println(nome)
+
+    nome = "Sergio Barbosa Ramos"
+
+    println(nome)
+
+    // val Imutável
+    val nome2 = "Coke"
+
+    // nome2 = "Pepsi" -> Error
+
+    println(nome2)
+}
