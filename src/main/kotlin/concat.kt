ec9051febc772985c6a00b136dc8dc1419850279
Sergio Ramos <sergio.b.snp@gmail.com>
2024-04-03 22:41:51 -0400

    Concatenação em Kotlin

---
+++ src/main/kotlin/concat.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var nome = "Sérgio"
+    var idade = 28
+    var profissao = "Software Engineer"
+
+    println("Nome: " + nome + ", Idade: " + idade + ", Profissão: " + profissao)
+    println("Nome: $nome, Idade: $idade, Profissão: $profissao")
+}
