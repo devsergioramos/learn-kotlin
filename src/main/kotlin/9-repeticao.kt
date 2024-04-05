a0f48cc5514201235293fd876fdd51a996ccb06c
Sergio Ramos <sergio.b.snp@gmail.com>
2024-04-04 21:12:49 -0400

    for

--- src/main/kotlin/9-repeticao.kt
+++ src/main/kotlin/9-repeticao.kt
@@ -5,8 +5,22 @@ fun main(args: Array<String>) {
 
     var indice = 0
 
+    println("Init while")
     while (indice < listaCompras.size) {
         println(listaCompras[indice])
         indice++
     }
+    println("End while")
+
+    println("Init for")
+    for (i in 0..3) {
+        println(listaCompras[i])
+    }
+    println("End for")
+
+    println("Init for")
+    for (i in listaCompras) {
+        println(i)
+    }
+    println("End for")
 }
