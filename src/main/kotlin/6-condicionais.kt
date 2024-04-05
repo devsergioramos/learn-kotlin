bda6f8d36aaf0d35da675f1b3a3df1d3014667a0
Sergio Ramos <sergio.b.snp@gmail.com>
2024-04-04 20:48:42 -0400

    condicionais

---
+++ src/main/kotlin/6-condicionais.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    var idade = 28
+    var maturidade: Byte
+
+    if (idade >= 28) {
+        maturidade = 70
+    } else if (idade >= 20 && idade < 28 ) {
+        maturidade = 40
+    } else if (idade >= 18 && idade < 20) {
+        maturidade = 20
+    } else {
+        maturidade = 0
+    }
+
+    println(maturidade)
+}
