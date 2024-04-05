04dbbc9f0f87964baa86c43520a06f935deaa9cb
Sergio Ramos <sergio.b.snp@gmail.com>
2024-04-04 21:17:49 -0400

    finish

---
+++ src/main/kotlin/10-when.kt
@@ -0,0 +1,23 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    var opcao = 6
+
+    when(opcao) {
+        1 -> println("Suco de laranja")
+        2 -> println("Suco de morango")
+        3 -> println("Suco de caju")
+        else -> println("Suco de fruta")
+    }
+
+    opcao = 1
+
+    var escolha = when(opcao) {
+        1 -> "Suco de laranja"
+        2 -> "Suco de morango"
+        3 -> "Suco de caju"
+        else -> "Suco de fruta"
+    }
+
+    println(escolha)
+}
