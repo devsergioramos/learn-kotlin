3b3d2b16b0729544972eab44f56fa4e172c6385c
Sergio Ramos <sergio.b.snp@gmail.com>
2024-04-04 21:04:54 -0400

    Arrays

---
+++ src/main/kotlin/8-array.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    var listaCompras = arrayOf("Arroz", "Feijão", "Frango", "Macarrão")
+
+    println(listaCompras[0])
+    println(listaCompras[1])
+    println(listaCompras[2])
+    println(listaCompras[3])
+
+    println(Arrays.toString(listaCompras))
+    println(listaCompras.size)
+}
