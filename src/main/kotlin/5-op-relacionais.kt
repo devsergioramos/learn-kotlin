369d8cf7937771212f8b1cae5097a40d703a1f11
Sergio Ramos <sergio.b.snp@gmail.com>
2024-04-04 20:41:05 -0400

    op relacionais

---
+++ src/main/kotlin/5-op-relacionais.kt
@@ -0,0 +1,28 @@
+fun main(args: Array<String>) {
+    var valor1 = 20
+    var valor2 = 10
+
+    // false
+    println(valor1 == valor2)
+
+    // true
+    println(valor1 != valor2)
+
+    // true
+    println(valor1 > valor2)
+
+    // false
+    println(valor1 < valor2)
+
+    // true
+    println(valor1 >= 20)
+
+    // false
+    println(valor1 > 20)
+
+    // true
+    println(valor1 <= 20)
+
+    // false
+    println(valor1 < 20)
+}
