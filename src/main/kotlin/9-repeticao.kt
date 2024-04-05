6f9b95a5c715ac32606c9c4297565548b47d9df3
Sergio Ramos <sergio.b.snp@gmail.com>
2024-04-04 21:08:40 -0400

    while

---
+++ src/main/kotlin/9-repeticao.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    var listaCompras = arrayOf("Arroz", "Feijão", "Frango", "Macarrão")
+
+    var indice = 0
+
+    while (indice < listaCompras.size) {
+        println(listaCompras[indice])
+        indice++
+    }
+}
