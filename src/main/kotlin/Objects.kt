2a7f6f626083c001f2d04ad926ef09bd360551d0
Sergio Ramos <sergio.b.snp@gmail.com>
2024-04-24 22:15:09 -0400

    create singleton objcts

---
+++ src/main/kotlin/Objects.kt
@@ -0,0 +1,14 @@
+object Global {
+    val PI = 3.14
+}
+
+fun main(args: Array<String>) {
+
+    val localObject = object {
+        val PI = 3.1416
+    }
+
+    println(localObject.PI)
+
+    println(Global.PI)
+}
