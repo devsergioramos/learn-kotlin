435fa373e8cd485cfe7a685945fccf40f5e52210
Sergio Ramos <sergio.ramos@loggi.com>
2024-05-15 23:55:35 -0300

    finish

--- src/main/kotlin/com/sergio/ramos/22-Tuples.kt
+++ src/main/kotlin/com/sergio/ramos/22-Tuples.kt
@@ -3,11 +3,11 @@ package com.sergio.ramos
 import java.io.Serializable
 
 fun capitalAndPopulation(country: String): Pair<String, Long> {
-    return Pair(country, 2024)
+    return Pair(country, 300000000)
 }
 
 fun countryInformation(country: String): Triple<String, String, Long> {
-    return Triple(country, "America", 2024)
+    return Triple(country, "America", 300000000)
 }
 
 // Its only data class
@@ -21,7 +21,7 @@ data class Fourth<out A, out B, out C, out D>(
 }
 
 fun stateInformation(country: String): Fourth<String, String, Long, String> {
-    return Fourth(country, "America", 2024, "BR")
+    return Fourth(country, "America", 300000000, "BR")
 }
 
 fun main() {
@@ -39,4 +39,24 @@ fun main() {
     println(resultStateInf.second)
     println(resultStateInf.third)
     println(resultStateInf.fourth)
+
+    val (capital, population) = capitalAndPopulation("Brazil")
+    println(capital)
+    println(population)
+
+    val (country, continent, population2) = countryInformation("Brazil")
+    println(country)
+    println(continent)
+    println(population2)
+
+    val (id, name, email) = CustomerDt(1, "Sergio", "sergio.b.snp@gmail.com")
+    println(id)
+    println(name)
+    println(email)
+
+    val listCapitalAndCountries = listOf(Pair("Madrid", "Spain"), "Paris" to "France")
+
+    for ((capital2, country2) in listCapitalAndCountries) {
+        println("$capital2 - $country2")
+    }
 }
