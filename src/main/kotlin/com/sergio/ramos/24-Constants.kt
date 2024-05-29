622deda135c2cbf2ee96709d4f05d9a06de00ad7
Sergio Ramos <sergio.ramos@loggi.com>
2024-05-28 21:45:25 -0300

    Constants

---
+++ src/main/kotlin/com/sergio/ramos/24-Constants.kt
@@ -0,0 +1,12 @@
+package com.sergio.ramos
+
+val COPYRIGHT_AUTHOR = "Sergio Ramos"
+
+object Copyright {
+    val author = "Sergio Ramos"
+}
+
+fun main() {
+    println("Author: ${Copyright.author}")
+    println(COPYRIGHT_AUTHOR)
+}
